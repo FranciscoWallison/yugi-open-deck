e1968b5f85b9cdcd6a19ac3aae1c0cef43e764a6
Francisco Wallison <franciscowallison@gmail.com>
2022-01-29 13:45:55 -0300

    routs

---
+++ YugiDeck/src/Routes.js
@@ -0,0 +1,32 @@
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+
+// import Login from './pages/LoginScreen';
+// import Formulario from './pages/Formulario';
+// import MenuInicial from './pages/MenuScreen';
+
+import InitiApp from './pages/InitiApp';
+
+let initRota = 'InitiApp';
+
+//Barra de GoBack
+const stackNav = createStackNavigator({
+//   MenuInicial,
+//   Formulario
+});
+
+export default (Routes = createAppContainer(
+  createSwitchNavigator(
+    {
+    //   Login: Login,
+      InitiApp: InitiApp,
+    //   Menu: MenuInicial,
+    //   Formulario: Formulario,
+      stackNav,
+    },
+    {
+      initialRouteName: initRota,
+    },
+  ),
+));
