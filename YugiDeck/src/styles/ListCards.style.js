2638e561f46e1f999ecdcafd668f80139ea86e3c
Francisco Wallison <franciscowallison@gmail.com>
2022-03-14 11:20:55 -0300

    remove commentarios

--- YugiDeck/src/styles/ListCards.style.js
+++ YugiDeck/src/styles/ListCards.style.js
@@ -1,17 +1,10 @@
 export default {
   item: {
-    // alignItems: "center",
-    // backgroundColor: "#dcda48",
-    // flexBasis: 0,
-    // flexGrow: 1,
      margin: 4,
-    // padding: 20
   },
   itemEmpty: {
-    // backgroundColor: "transparent"
   },
   text: {
-    // color: "#333333"
   }
 
 }
